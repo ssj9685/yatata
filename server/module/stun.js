a32f97ff9fdbffc0edb7075aafa343b8cab41132
shinseokjin <ssj9685@naver.com>
2021-05-26 23:56:42 +0900

    Modify stun response test

--- server/module/stun.js
+++ server/module/stun.js
@@ -10,7 +10,10 @@ udpSocket.on('message', (udpMessage, rinfo) => {
     console.log(udpMessage, rinfo);
     const header = udpMessage;
     header.writeUInt16BE(0x0101,0);
-    udpSocket.send(header,rinfo.port,rinfo.address);
+    const address = Buffer.from(JSON.stringify(rinfo));
+    header.writeUInt16BE(address.length, 2);
+    let sendBuf = Buffer.concat([header, address]);
+    udpSocket.send(sendBuf, rinfo.port, rinfo.address);
 });
 
 udpSocket.on('listening',()=>{
