55d3f78d89c4e8bc0503053d6b33cc74be094f73
shinseokjin <ssj9685@naver.com>
2021-06-02 16:58:36 +0900

    Modify stun xor mapped

--- server/module/stun.js
+++ server/module/stun.js
@@ -2,17 +2,27 @@ const dgram = require('dgram');
 
 const udpSocket = dgram.createSocket('udp4');
 
+const magicCookieBuf = Buffer.alloc(4);
+magicCookieBuf.writeUIntBE(0x2112a442,0,4);
+const msb = magicCookieBuf.readUInt16BE(0);
+
 udpSocket.on('error', err => console.log(err));
 
 udpSocket.on('message', (udpMessage, rinfo) => {
     console.log(udpMessage, rinfo);
     const header = udpMessage;
-    header.writeUInt16BE(0x0101,0);
-    header.writeUInt16BE(address.length, 2);
-    //const attrValue = Buffer.alloc(8);
-    //attrValue.writeUIntBE(0x00,0,1);
-    //const attribute = Buffer.alloc()
-    let sendBuf = Buffer.concat([header, address]);
+    header.writeUIntBE(0x0101,0, 2);
+    header.writeUIntBE(12,2,2);
+    const attrValue = Buffer.alloc(8);
+    attrValue.writeUIntBE(0x01,1,1);
+    attrValue.writeUIntBE(rinfo.port ^ msb, 2, 2);
+    const ip = rinfo.address.split('.');
+    for(let i=0,j=ip.length;i<j;++i)
+        attrValue.writeUIntBE(Number(ip[i])^magicCookieBuf[i],i+4,1);
+    const attribute = Buffer.alloc(4);
+    attribute.writeUIntBE(0x0020,0,2);
+    attribute.writeUIntBE(8,2,2);
+    let sendBuf = Buffer.concat([header, attribute, attrValue]);
     udpSocket.send(sendBuf, rinfo.port, rinfo.address);
 });
 
