c661e878c9f6430f2e7efd0d1e7f16970b431b94
Seokjin Shin <ssj9685@naver.com>
2021-06-03 15:57:17 +0900

    Update stun to module

--- server/module/stun.js
+++ server/module/stun.js
@@ -1,33 +1,30 @@
 const dgram = require('dgram');
 
-const udpSocket = dgram.createSocket('udp4');
+module.exports = function Stun(){
+    this.udpSocket = dgram.createSocket('udp4');
 
-const magicCookieBuf = Buffer.alloc(4);
-magicCookieBuf.writeUIntBE(0x2112a442,0,4);
-const msb = magicCookieBuf.readUInt16BE(0);
+    const magicCookieBuf = Buffer.from('2112a442','hex')
+    const msb = magicCookieBuf.readUInt16BE(0); //most significant 16 bits
 
-udpSocket.on('error', err => console.log(err));
+    this.udpSocket.on('error', err => console.log(err));
 
-udpSocket.on('message', (udpMessage, rinfo) => {
-    console.log(udpMessage, rinfo);
-    const header = udpMessage;
-    header.writeUIntBE(0x0101,0, 2);
-    header.writeUIntBE(12,2,2);
-    const attrValue = Buffer.alloc(8);
-    attrValue.writeUIntBE(0x01,1,1);
-    attrValue.writeUIntBE(rinfo.port ^ msb, 2, 2);
-    const ip = rinfo.address.split('.');
-    for(let i=0,j=ip.length;i<j;++i)
-        attrValue.writeUIntBE(Number(ip[i])^magicCookieBuf[i],i+4,1);
-    const attribute = Buffer.alloc(4);
-    attribute.writeUIntBE(0x0020,0,2);
-    attribute.writeUIntBE(8,2,2);
-    let sendBuf = Buffer.concat([header, attribute, attrValue]);
-    udpSocket.send(sendBuf, rinfo.port, rinfo.address);
-});
-
-udpSocket.on('close', () => console.log('closed'));
-
-udpSocket.bind(41234,()=>{
-    console.log(udpSocket.address());
-});
+    this.udpSocket.on('message', (udpMessage, rinfo) => {
+        console.log(udpMessage, rinfo);
+        const header = udpMessage;
+        header.writeUIntBE(0x0101,0, 2); //success response
+        header.writeUIntBE(12,2,2);
+        const attrValue = Buffer.alloc(8);
+        attrValue.writeUIntBE(0x01,1,1);
+        attrValue.writeUIntBE(rinfo.port ^ msb, 2, 2);
+        const ip = rinfo.address.split('.');
+        for(let i=0,j=ip.length;i<j;++i)
+            attrValue.writeUIntBE(Number(ip[i])^magicCookieBuf[i],i+4,1);
+        const attribute = Buffer.alloc(4);
+        attribute.writeUIntBE(0x0020,0,2); //attribute type
+        attribute.writeUIntBE(8,2,2);
+        let sendBuf = Buffer.concat([header, attribute, attrValue]);
+        udpSocket.send(sendBuf, rinfo.port, rinfo.address);
+    });
+    
+    this.udpSocket.bind(41234);
+}
