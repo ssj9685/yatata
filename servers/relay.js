aa42cf0f9665a8bb24fd9a24f0e96b03ff2e12fb
Shin Seokjin <ssj9685@naver.com>
2021-06-14 02:39:31 +0900

    Add relay server

--- servers/relay.js
+++ servers/relay.js
@@ -8,11 +8,33 @@ module.exports = function Relay(){
 
     this.udpSocket.on('message', (udpMessage, rinfo) => {
         const fullAddress = `${rinfo.address}:${rinfo.port}`;
-        this.sockets.set(fullAddress, udpMessage);
-        for(const socket of this.sockets){
-            const [fullAddr, message] = socket;
-            if(fullAddr !== fullAddress){
-                this.udpSocket.send(message, rinfo.port, rinfo.address);
+        if(!this.sockets.has(fullAddress)){
+            this.sockets.set(fullAddress, [udpMessage]);
+        }
+        else{
+            const messages = this.sockets.get(fullAddress);
+            const lastMessage = messages[messages.length - 1];
+            if(lastMessage){
+                if(Buffer.compare(lastMessage, udpMessage) !== 0){
+                    messages.push(udpMessage);
+                }
+            }
+            else{
+                messages.push(udpMessage);
+            }
+            if(this.sockets.size === 2){
+                let peers = [];
+                for(const socket of this.sockets){
+                   peers.push(socket)
+                }
+                const [fa1, m1] = peers[0];
+                const [a1, p1] = fa1.split(':');
+                const [fa2, m2] = peers[1];
+                const [a2, p2] = fa2.split(':');
+                if(m1.length > 0)
+                this.udpSocket.send(m1.shift(), Number(p2), a2);
+                if(m2.length > 0)
+                this.udpSocket.send(m2.shift(), Number(p1), a1);
             }
         }
     });
